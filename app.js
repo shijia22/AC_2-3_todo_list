ae96ea952ef035fab59b7f01ac6df7194fc92fd8
shichia22 <shichia0207@gmail.com>
2021-07-11 23:06:18 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
+
+const Todo = require('./models/todo')
 const app = express()
 
 mongoose.connect('mongodb://localhost/todo-list', {
@@ -26,7 +28,10 @@ app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then((todos) => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch((error) => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000
