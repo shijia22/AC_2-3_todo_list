563ffe8e1f36e402b59923abc06ad0d519596535
shichia22 <shichia0207@gmail.com>
2021-07-13 00:02:23 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override') 
 
 const Todo = require('./models/todo')
 const routes = require('./routes') // 引用路由器
+
+require('./config/mongoose')
+
 const app = express()
-
-mongoose.connect('mongodb://localhost/todo-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,18 +1,6 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo') // 載入 todo model
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/todo-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 // 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
